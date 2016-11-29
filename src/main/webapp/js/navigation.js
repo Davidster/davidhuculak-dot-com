6a00353bf14d4c3a6fd7ffa728a479b4bb1699d7
David Huculak <davidhuculak5@gmail.com>
2016-11-29 00:40:58 -0500

    linked personal image to google+ profile

--- src/main/webapp/js/navigation.js
+++ src/main/webapp/js/navigation.js
@@ -1,7 +1,11 @@
 $( document ).ready(function() {
 
-    $(".label").on('click', function() {
+    $(".label").on("click", function() {
         $("#hey-thats-pretty-good").text($(this).text());
+    });
+
+    $("#personal-image").on("click", function() {
+        window.open("https://plus.google.com/u/0/108745335875684184968");
     });
 
 });
