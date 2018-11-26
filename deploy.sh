899738a46abdb278a669c93bdc354f5c62c835be
David Huculak <davidhuculak5@gmail.com>
2018-11-25 20:56:45 -0500

    added cloudformation template for deploying to aws

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 eval $(assume-role david)
 
-aws s3 sync ./ s3://davidhuculak.com --exclude ".git/*" --exclude ".gitignore" --exclude "README.md"
+aws s3 sync ./ s3://davidhuculak-dot-com-staticclientbucket-3ahpjym8isbn --exclude ".git/*" --exclude ".gitignore" --exclude "README.md" --exclude "cf-template.template"
