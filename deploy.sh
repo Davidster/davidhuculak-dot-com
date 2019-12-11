83c820d4fa1db6d7fb74180fd960705dc257a996
David Huculak <davidhuculak5@gmail.com>
2019-12-11 14:41:39 -0500

    add Cache-Control: no-cache for html files

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-aws s3 sync --profile default \
-  ./ s3://david-huculak-dot-com-staticclientbucket-1u9s1y54u0ymv \
+S3_BUCKET_NAME="david-huculak-dot-com-staticclientbucket-1u9s1y54u0ymv"
+
+aws s3 sync ./ s3://$S3_BUCKET_NAME \
   --delete \
+  --exclude "*.html" \
   --exclude ".git/*" \
   --exclude ".gitignore" \
   --exclude "README.md" \
   --exclude "deploy.sh" \
   --exclude "cloudformation.yaml"
+
+# include Cache-Control: no-cache for html files
+aws s3 cp ./ s3://$S3_BUCKET_NAME \
+  --recursive \
+  --exclude "*" \
+  --include "*.html" \
+  --cache-control "no-cache"
